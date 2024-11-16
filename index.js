55ad793ec462c382a0cd8c95fbb7e96e78f04bf1
cornel05 <ngotrandinhduy2019@gmail.com>
2024-11-16 09:46:28 +0700

    random stuff

--- index.js
+++ index.js
@@ -1,53 +1,55 @@
-function playSound(key){
-    var audio;
-    switch (key){
-        case 'w':
-            audio = new Audio('sounds/tom-1.mp3');
-            break;
-        case 'a':
-            audio = new Audio('sounds/tom-2.mp3');
-            break;
-        case 's':
-            audio = new Audio('sounds/tom-3.mp3');
-            break;
-        case 'd':
-            audio = new Audio('sounds/tom-4.mp3');
-            break;
-        case 'j':
-            audio = new Audio('sounds/snare.mp3');
-            break;
-        case 'k':
-            audio = new Audio('sounds/crash.mp3');;
-            break;
-        case 'l':
-            audio = new Audio('sounds/kick-bass.mp3');
-            break;
-    }
-    audio.play();
-    animateButton(key);
+function playSound(key) {
+  var audio;
+  switch (key) {
+    case "w":
+      audio = new Audio("sounds/tom-1.mp3");
+      break;
+    case "a":
+      audio = new Audio("sounds/tom-2.mp3");
+      break;
+    case "s":
+      audio = new Audio("sounds/tom-3.mp3");
+      break;
+    case "d":
+      audio = new Audio("sounds/tom-4.mp3");
+      break;
+    case "j":
+      audio = new Audio("sounds/snare.mp3");
+      break;
+    case "k":
+      audio = new Audio("sounds/crash.mp3");
+      break;
+    case "l":
+      audio = new Audio("sounds/kick-bass.mp3");
+      break;
+  }
+  audio.play();
+  animateButton(key);
 }
 
-function animateButton(key){
-    var button = document.querySelector("." + key);
-    if (button){
-        button.classList.add("animated");
-        setTimeout(function (){
-            button.classList.remove("animated");
-        }, 100);
-    }
+function animateButton(key) {
+  var button = document.querySelector("." + key);
+  if (button) {
+    button.classList.add("animated");
+    setTimeout(function () {
+      button.classList.remove("animated");
+    }, 100);
+  }
 }
 
-document.addEventListener("keydown", function (event) {
+window.addEventListener("keydown", function (event) {
+    console.log(event);
   playSound(event.key);
 });
 
 var buttons = document.querySelectorAll("button");
-buttons.forEach(function(btn) {
-    btn.addEventListener("click", function () {
-        playSound(this.innerHTML);
-    })
+buttons.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    playSound(this.innerHTML);
+  });
 });
 
 window.addEventListener("beforeunload", function (event) {
-    event.preventDefault();
+    console.log(event);
+  event.preventDefault();
 });
